24f9ea1ebc6ddbcbe94b7692e0f9548a0184c7bd
onwd <onwdit@gmail.com>
2019-05-24 02:20:59 +0600

    Added "start" and "stop" methods to Master

--- source/master.js
+++ source/master.js
@@ -1,7 +1,27 @@
+const WebSocket = require('ws');
+
 class Master {
   constructor(options) {
     this.port = options.port || 9000;
     this.tasks = options.tasks || [];
+    this.wss = null;
+  }
+
+  start() {
+    if (this.wss) {
+      return;
+    }
+
+    this.wss = new WebSocket.Server({
+      port: this.port
+    });
+  }
+
+  stop() {
+    if (this.wss) {
+      this.wss.close();
+      this.wss = null;
+    }
   }
 }
 
