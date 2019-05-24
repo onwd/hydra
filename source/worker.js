8494ad8a7396c5cdfb3083c8db8b75e1e70f0db8
onwd <onwdit@gmail.com>
2019-05-24 23:07:03 +0600

    Implement Worker start and stop methods

--- source/worker.js
+++ source/worker.js
@@ -1,11 +1,44 @@
+const WebSocket = require('ws');
+
 class Worker {
   constructor(options) {
     this.url = options.url || 'wss://localhost:9000';
+    this.wss = null;
   }
 
-  start() { }
+  start() {
+    this.connect();
+  }
 
-  stop() { }
+  stop() {
+    this.disconnect();
+  }
+
+  connect() {
+    if (this.wss) {
+      return;
+    }
+
+    this.wss = new WebSocket(this.url);
+
+    this.wss.on('open', this.onConnected.bind(this));
+    this.wss.on('message', this.onMessageReceived.bind(this));
+  }
+
+  disconnect() {
+    if (this.wss) {
+      this.wss.close();
+      this.wss = null;
+    }
+  }
+
+  onConnected() {
+    console.log('Connected to master');
+  }
+
+  onMessageReceived(data) {
+    console.log('Received message from master', data);
+  }
 }
 
 module.exports = Worker;
