a9d0d7924db4072daaa45b1190881bcd30dd79d6
onwd <onwdit@gmail.com>
2019-05-25 01:31:48 +0600

    Added getSubspace parameter to Task

--- source/task.ts
+++ source/task.ts
@@ -4,10 +4,12 @@ export default class Task {
   public dependencies: Array<string>;
   public f: Function;
   public space: Space;
+  public getSubspace: Function;
 
   constructor(options: any) {
     this.dependencies = options.dependencies || [];
     this.f = options.f || null;
     this.space = options.space || null;
+    this.getSubspace = options.getSubspace || null;
   }
 }
