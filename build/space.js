6e197ff0dc39968840ef4bfb211829dc67433444
onwd <onwdit@gmail.com>
2019-05-29 23:54:49 +0600

    Rename getNextValue to getNext

--- build/space.js
+++ build/space.js
@@ -2,6 +2,6 @@ export default class Space {
     constructor(options) {
         this.type = options.type || 'array';
         this.values = options.values || [];
-        this.getNextValue = options.getNextValue || null;
+        this.getNext = options.getNext || null;
     }
 }
--- source/space.ts
+++ source/space.ts
@@ -1,11 +1,11 @@
 export default class Space {
   public type: string;
   public values: Array<any>;
-  public getNextValue: Function;
+  public getNext: Function;
 
   constructor(options: any) {
     this.type = options.type || 'array';
     this.values = options.values || [];
-    this.getNextValue = options.getNextValue || null;
+    this.getNext = options.getNext || null;
   }
 }
