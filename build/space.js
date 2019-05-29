308a38ed5324a803d815f279314d3b70d50c3354
onwd <onwdit@gmail.com>
2019-05-29 23:58:34 +0600

    Revert "Rename getNextValue to getNext"
    This reverts commit 6e197ff0dc39968840ef4bfb211829dc67433444.

--- build/space.js
+++ build/space.js
@@ -2,6 +2,6 @@ export default class Space {
     constructor(options) {
         this.type = options.type || 'array';
         this.values = options.values || [];
-        this.getNext = options.getNext || null;
+        this.getNextValue = options.getNextValue || null;
     }
 }
--- source/space.ts
+++ source/space.ts
@@ -1,11 +1,11 @@
 export default class Space {
   public type: string;
   public values: Array<any>;
-  public getNext: Function;
+  public getNextValue: Function;
 
   constructor(options: any) {
     this.type = options.type || 'array';
     this.values = options.values || [];
-    this.getNext = options.getNext || null;
+    this.getNextValue = options.getNextValue || null;
   }
 }
