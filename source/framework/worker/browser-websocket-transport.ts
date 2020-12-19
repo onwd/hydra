b23befac3e29bbe727faae0e1ed87ad830ccd7ec
onwd <onwdit@gmail.com>
2020-12-19 17:13:52 +0600

    Added BrowserWebsocketTransport

---
+++ source/framework/worker/browser-websocket-transport.ts
@@ -0,0 +1,36 @@
+import { Message } from '../core/message';
+import { Transport } from './transport';
+
+export class BrowserWebsocketTransport extends Transport {
+  public url: string;
+  public ws: WebSocket;
+
+  constructor(transport?: Partial<BrowserWebsocketTransport>) {
+    super(transport);
+
+    this.url = transport?.url ?? 'ws://localhost:9000';
+    this.ws = transport?.ws ?? null;
+  }
+
+  public connect(): void {
+    if (!this.ws) {
+      this.ws = new WebSocket(this.url);
+
+      this.ws.onopen = this.handleConnected.bind(this);
+      this.ws.onmessage = this.handleMessageReceived.bind(this);
+    }
+  }
+
+  public disconnect(): void {
+    if (this.ws) {
+      this.ws.close();
+      this.ws = null;
+    }
+  }
+
+  public send(event: string, data?: any): void {
+    const message = new Message({ event, data });
+
+    this.ws.send(message.serialize());
+  }
+}
